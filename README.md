3f1d96424947b45458348cb6c1f3c75e225207fc
vodoo13 <hasssanboy78@gmail.com>
2024-02-26 20:41:41 +0300

    new game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/vodoo13/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/vodoo13/python-project-49/actions)
 <a href="https://codeclimate.com/github/vodoo13/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/d0e2c19f3bb3dab4096f/maintainability" /></a>
+https://asciinema.org/a/iaSt7G5NXIFlVZPPwq5XBiUQf
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,36 @@
+import random
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    correct_answers_needed = 3
+    correct_answers = 0
+
+    while correct_answers < correct_answers_needed:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        user_answer = input("Your answer: ").lower()
+
+        if (is_even(number) and user_answer == "yes") or (not is_even(number) and user_answer == "no"):
+            print("Correct!")
+            correct_answers += 1
+        else:
+            correct_answer = "yes" if is_even(number) else "no"
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+
+    if correct_answers == correct_answers_needed:
+        print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
