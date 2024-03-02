da76cb00aa38df090601be84517d397fe10e8d3e
vodoo13 <hasssanboy78@gmail.com>
2024-03-02 21:53:12 +0300

    FIXXX

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -7,7 +7,7 @@ def get_game():
     start = random.randint(1, 30)
     progression_length = random.randint(6, 10)
     diff = random.randint(1, 5)
-    cut_position = random.randint(0, k - 1)
+    cut_position = random.randint(0, progression_length - 1)
     result = []
     for i in range(progression_length):
         result.append(start + i * diff)
