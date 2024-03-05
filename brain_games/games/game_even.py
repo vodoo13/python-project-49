b6a3f946c87cad8ad0b5df7dc22b5e1de35ba7e6
vodoo13 <hasssanboy78@gmail.com>
2024-03-05 10:38:23 +0300

    fix brain-prime and brain-even

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -3,14 +3,11 @@ GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_number_even(number):
-    if number % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
+    return number % 2 == 0
 
 
 def get_game():
     number = random.randint(1, 100)
     question = f"Question: {number}"
-    result = is_number_even(number)
+    result = 'yes' if is_number_even(number) else 'no'
     return question, result
--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -6,17 +6,17 @@ GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_number_prime(random_number):
     if random_number <= 1:
-        return "no"
+        return False
 
     for i in range(2, int(math.sqrt(random_number)) + 1):
         if random_number % i == 0:
-            return "no"
+            return False
 
-    return "yes"
+    return True
 
 
 def get_game():
     random_number = random.randint(2, 100)
     question = f"Question: {random_number}"
-    result = is_number_prime(random_number)
+    result = 'yes' if is_number_prime(random_number) else 'no'
     return question, result
